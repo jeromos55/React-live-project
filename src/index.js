6cebad9c35fa23710443f1e2a4ae0dde70628997
jeromos55 <jeromos55@gmail.com>
2023-10-31 15:23:55 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 
-// import App from "./App";
-// import "./styles.css";
-import StarRating from "./StarRating";
+import App from "./App";
+import "./styles.css";
+// import StarRating from "./StarRating";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars.</p>
+//     </div>
+//   );
+// }
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={3} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <App />
+    {/* <StarRating maxRating={5} />
+    <StarRating size={24} color="black" className="test" defaultRating={3} />
+    <Test /> */}
   </StrictMode>
 );
